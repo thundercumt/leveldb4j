ecb553a69de05c9bb473ecae1fa67c18e8ffaca7
thundercumt <thundercumt@126.com>
2017-01-22 23:55:01 +0800

    skipAdd alpha version

--- src/main/java/leveldb4j/db/SkipList.java
+++ src/main/java/leveldb4j/db/SkipList.java
@@ -29,14 +29,14 @@ public class SkipList<K extends Comparable<K>, V> {
 
     private Random     rand;
     private K          leftInf, rightInf;
-    private Node<K, V> topLeft;
+    private Node<K, V> topLeft, topRight;
 
     public SkipList(K leftBound, K rightBound) {
         rand = new Random(System.currentTimeMillis());
         leftInf = leftBound;
         rightInf = rightBound;
-        topLeft = new Node<K, V>(leftInf, null);
-        Node<K, V> topRight = new Node<K, V>(rightInf, null);
+        topLeft = new Node<>(leftInf, null);
+        topRight = new Node<>(rightInf, null);
         topLeft.next = topRight;
         topRight.prev = topLeft;
     }
@@ -63,12 +63,52 @@ public class SkipList<K extends Comparable<K>, V> {
             return find;
         }
 
-        Node<K, V> insert = new Node<K, V>(key, value);
+        Node<K, V> insert = new Node<>(key, value);
         insert.next = find.next;
         find.next.prev = insert;
         insert.prev = find;
         find.next = insert;
 
+        find = insert;
+        while (rand.nextBoolean()) {
+            while (find != null && find.above == null)
+                find = find.prev;
+
+            if (find == null) {
+                growEndianTower();
+                find = topLeft;
+            } else {
+                find = find.above;
+            }
+
+            Node<K, V> towerTop = new Node<>(key, value);
+            insert.above = towerTop;
+            towerTop.below = insert;
+            towerTop.next = find.next;
+            find.next.prev = towerTop;
+            find.next = towerTop;
+            towerTop.prev = find;
+
+            insert = towerTop;
+            find = insert;
+        }
+
+        return insert;
+    }
+
+    private void growEndianTower() {
+        Node<K, V> topLeft = new Node<>(leftInf, null);
+        Node<K, V> topRight = new Node<>(rightInf, null);
+        topLeft.next = topRight;
+        topRight.prev = topLeft;
+
+        this.topLeft.above = topLeft;
+        topLeft.below = this.topLeft;
+        this.topLeft = topLeft;
+
+        this.topRight.above = topRight;
+        topRight.below = this.topRight;
+        this.topRight = topRight;
     }
 
 }
